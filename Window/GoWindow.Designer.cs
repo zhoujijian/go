c5244e89bc5525aadcbac202bd729445f30564cc
Hamp <qq111495@163.com>
2017-07-21 10:15:08 +0800

    set language from chinese to english

--- Window/GoWindow.Designer.cs
+++ Window/GoWindow.Designer.cs
@@ -42,8 +42,8 @@
             this.butRemVariety = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panelVariety = new System.Windows.Forms.Panel();
-            this.panelRoot = new System.Windows.Forms.Panel();
             this.lstVariety = new System.Windows.Forms.ListBox();
+            this.panelRoot = new System.Windows.Forms.Panel();
             this.topmenu.SuspendLayout();
             this.panel1.SuspendLayout();
             this.panelVariety.SuspendLayout();
@@ -58,7 +58,8 @@
             this.aboutToolStripMenuItem});
             this.topmenu.Location = new System.Drawing.Point(0, 0);
             this.topmenu.Name = "topmenu";
-            this.topmenu.Size = new System.Drawing.Size(1018, 25);
+            this.topmenu.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
+            this.topmenu.Size = new System.Drawing.Size(1096, 24);
             this.topmenu.TabIndex = 0;
             this.topmenu.Text = "menuStrip1";
             // 
@@ -68,73 +69,76 @@
             this.newKifu,
             this.openKifu,
             this.saveKifu});
+            this.fileToolStripMenuItem.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.fileToolStripMenuItem.Text = "棋谱";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(47, 21);
+            this.fileToolStripMenuItem.Text = "Kifu";
             // 
             // newKifu
             // 
             this.newKifu.Name = "newKifu";
-            this.newKifu.Size = new System.Drawing.Size(100, 22);
-            this.newKifu.Text = "新建";
+            this.newKifu.Size = new System.Drawing.Size(152, 22);
+            this.newKifu.Text = "New";
             // 
             // openKifu
             // 
             this.openKifu.Name = "openKifu";
-            this.openKifu.Size = new System.Drawing.Size(100, 22);
-            this.openKifu.Text = "打开";
+            this.openKifu.Size = new System.Drawing.Size(152, 22);
+            this.openKifu.Text = "Open";
             // 
             // saveKifu
             // 
             this.saveKifu.Name = "saveKifu";
-            this.saveKifu.Size = new System.Drawing.Size(100, 22);
-            this.saveKifu.Text = "保存";
+            this.saveKifu.Size = new System.Drawing.Size(152, 22);
+            this.saveKifu.Text = "Save";
             // 
             // optionToolStripMenuItem
             // 
+            this.optionToolStripMenuItem.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.optionToolStripMenuItem.Name = "optionToolStripMenuItem";
-            this.optionToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.optionToolStripMenuItem.Text = "选项";
+            this.optionToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.optionToolStripMenuItem.Text = "Option";
             // 
             // aboutToolStripMenuItem
             // 
+            this.aboutToolStripMenuItem.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(44, 21);
-            this.aboutToolStripMenuItem.Text = "关于";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.aboutToolStripMenuItem.Text = "About";
             // 
             // panelLayer
             // 
             this.panelLayer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panelLayer.Location = new System.Drawing.Point(5, 3);
+            this.panelLayer.Location = new System.Drawing.Point(6, 3);
             this.panelLayer.Name = "panelLayer";
-            this.panelLayer.Size = new System.Drawing.Size(570, 570);
+            this.panelLayer.Size = new System.Drawing.Size(574, 574);
             this.panelLayer.TabIndex = 1;
             // 
             // textComment
             // 
             this.textComment.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.textComment.Location = new System.Drawing.Point(3, 33);
+            this.textComment.Location = new System.Drawing.Point(3, 29);
             this.textComment.Multiline = true;
             this.textComment.Name = "textComment";
-            this.textComment.Size = new System.Drawing.Size(434, 230);
+            this.textComment.Size = new System.Drawing.Size(506, 249);
             this.textComment.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label1.Location = new System.Drawing.Point(83, 6);
+            this.label1.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(97, 7);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 14);
+            this.label1.Size = new System.Drawing.Size(56, 17);
             this.label1.TabIndex = 3;
-            this.label1.Text = "执黑：";
+            this.label1.Text = "Black:";
             // 
             // butResearch
             // 
             this.butResearch.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butResearch.Location = new System.Drawing.Point(291, 3);
+            this.butResearch.Location = new System.Drawing.Point(291, 4);
             this.butResearch.Name = "butResearch";
-            this.butResearch.Size = new System.Drawing.Size(122, 30);
+            this.butResearch.Size = new System.Drawing.Size(124, 35);
             this.butResearch.TabIndex = 9;
             this.butResearch.Text = "Research";
             this.butResearch.UseVisualStyleBackColor = true;
@@ -142,9 +146,9 @@
             // butNext
             // 
             this.butNext.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butNext.Location = new System.Drawing.Point(153, 4);
+            this.butNext.Location = new System.Drawing.Point(152, 3);
             this.butNext.Name = "butNext";
-            this.butNext.Size = new System.Drawing.Size(122, 30);
+            this.butNext.Size = new System.Drawing.Size(119, 35);
             this.butNext.TabIndex = 2;
             this.butNext.Text = "Next";
             this.butNext.UseVisualStyleBackColor = true;
@@ -152,9 +156,9 @@
             // butSaveResearch
             // 
             this.butSaveResearch.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butSaveResearch.Location = new System.Drawing.Point(430, 3);
+            this.butSaveResearch.Location = new System.Drawing.Point(434, 3);
             this.butSaveResearch.Name = "butSaveResearch";
-            this.butSaveResearch.Size = new System.Drawing.Size(129, 30);
+            this.butSaveResearch.Size = new System.Drawing.Size(133, 35);
             this.butSaveResearch.TabIndex = 1;
             this.butSaveResearch.Text = "Save";
             this.butSaveResearch.UseVisualStyleBackColor = true;
@@ -162,9 +166,9 @@
             // butPrev
             // 
             this.butPrev.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.butPrev.Location = new System.Drawing.Point(13, 3);
+            this.butPrev.Location = new System.Drawing.Point(5, 3);
             this.butPrev.Name = "butPrev";
-            this.butPrev.Size = new System.Drawing.Size(124, 30);
+            this.butPrev.Size = new System.Drawing.Size(125, 35);
             this.butPrev.TabIndex = 0;
             this.butPrev.Text = "Prev";
             this.butPrev.UseVisualStyleBackColor = true;
@@ -172,31 +176,31 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.label2.Location = new System.Drawing.Point(266, 6);
+            this.label2.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(310, 7);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 14);
+            this.label2.Size = new System.Drawing.Size(56, 17);
             this.label2.TabIndex = 5;
-            this.label2.Text = "执白：";
+            this.label2.Text = "White:";
             // 
             // butSetNote
             // 
-            this.butSetNote.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.butSetNote.Location = new System.Drawing.Point(3, 267);
+            this.butSetNote.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butSetNote.Location = new System.Drawing.Point(3, 280);
             this.butSetNote.Name = "butSetNote";
-            this.butSetNote.Size = new System.Drawing.Size(434, 30);
+            this.butSetNote.Size = new System.Drawing.Size(506, 35);
             this.butSetNote.TabIndex = 7;
-            this.butSetNote.Text = "保  存  解  说";
+            this.butSetNote.Text = "Save Note";
             this.butSetNote.UseVisualStyleBackColor = true;
             // 
             // butRemVariety
             // 
-            this.butRemVariety.Font = new System.Drawing.Font("宋体", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
-            this.butRemVariety.Location = new System.Drawing.Point(3, 578);
+            this.butRemVariety.Font = new System.Drawing.Font("Consolas", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.butRemVariety.Location = new System.Drawing.Point(3, 582);
             this.butRemVariety.Name = "butRemVariety";
-            this.butRemVariety.Size = new System.Drawing.Size(434, 29);
+            this.butRemVariety.Size = new System.Drawing.Size(506, 34);
             this.butRemVariety.TabIndex = 8;
-            this.butRemVariety.Text = "删  除  变  化";
+            this.butRemVariety.Text = "Remove Variety";
             this.butRemVariety.UseVisualStyleBackColor = true;
             // 
             // panel1
@@ -206,9 +210,9 @@
             this.panel1.Controls.Add(this.butPrev);
             this.panel1.Controls.Add(this.butSaveResearch);
             this.panel1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel1.Location = new System.Drawing.Point(5, 578);
+            this.panel1.Location = new System.Drawing.Point(6, 582);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(570, 35);
+            this.panel1.Size = new System.Drawing.Size(574, 41);
             this.panel1.TabIndex = 0;
             // 
             // panelVariety
@@ -219,21 +223,10 @@
             this.panelVariety.Controls.Add(this.textComment);
             this.panelVariety.Controls.Add(this.butSetNote);
             this.panelVariety.Controls.Add(this.label2);
-            this.panelVariety.Location = new System.Drawing.Point(578, 3);
+            this.panelVariety.Location = new System.Drawing.Point(583, 3);
             this.panelVariety.Name = "panelVariety";
-            this.panelVariety.Size = new System.Drawing.Size(440, 610);
+            this.panelVariety.Size = new System.Drawing.Size(513, 620);
             this.panelVariety.TabIndex = 9;
-            // 
-            // panelRoot
-            // 
-            this.panelRoot.Controls.Add(this.panelLayer);
-            this.panelRoot.Controls.Add(this.panelVariety);
-            this.panelRoot.Controls.Add(this.panel1);
-            this.panelRoot.Enabled = false;
-            this.panelRoot.Location = new System.Drawing.Point(-1, 28);
-            this.panelRoot.Name = "panelRoot";
-            this.panelRoot.Size = new System.Drawing.Size(1021, 616);
-            this.panelRoot.TabIndex = 10;
             // 
             // lstVariety
             // 
@@ -241,22 +234,34 @@
             this.lstVariety.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lstVariety.FormattingEnabled = true;
             this.lstVariety.ItemHeight = 14;
-            this.lstVariety.Location = new System.Drawing.Point(3, 303);
+            this.lstVariety.Location = new System.Drawing.Point(3, 322);
             this.lstVariety.Name = "lstVariety";
-            this.lstVariety.Size = new System.Drawing.Size(434, 268);
+            this.lstVariety.Size = new System.Drawing.Size(506, 254);
             this.lstVariety.TabIndex = 9;
+            // 
+            // panelRoot
+            // 
+            this.panelRoot.Controls.Add(this.panelLayer);
+            this.panelRoot.Controls.Add(this.panelVariety);
+            this.panelRoot.Controls.Add(this.panel1);
+            this.panelRoot.Enabled = false;
+            this.panelRoot.Location = new System.Drawing.Point(-1, 33);
+            this.panelRoot.Name = "panelRoot";
+            this.panelRoot.Size = new System.Drawing.Size(1099, 624);
+            this.panelRoot.TabIndex = 10;
             // 
             // GoWindow
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1018, 644);
+            this.ClientSize = new System.Drawing.Size(1096, 658);
             this.Controls.Add(this.panelRoot);
             this.Controls.Add(this.topmenu);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.MainMenuStrip = this.topmenu;
             this.MaximizeBox = false;
             this.Name = "GoWindow";
-            this.Text = "乐扬围棋";
+            this.Text = "Go-LY";
             this.topmenu.ResumeLayout(false);
             this.topmenu.PerformLayout();
             this.panel1.ResumeLayout(false);
